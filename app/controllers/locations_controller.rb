beed478355b5edf7f58a10c420e0d2c1c7e1ece7
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-07-19 16:33:57 -0400

    added foundation gem to app

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -13,8 +13,8 @@ class LocationsController < ApplicationController
 
   # GET /locations/1
   # GET /locations/1.json
-  # def show
-  # end
+  def show
+  end
 
   # # GET /locations/new
   # def new
@@ -65,11 +65,11 @@ class LocationsController < ApplicationController
   #   end
   # end
 
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_location
-  #     @location = Location.find(params[:id])
-  #   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
 
   resources :user_sessions
   resources :users
+  resources :locations
   resources :categories, only: [:show, :index]
   resources :restaurants do
     resources :reservations, only: [:index]
   end
-  resources :locations
+
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
